4051df96e5fbd479bc6a20aeeda24fe9582d0e19
MkGodh <fenryrvla@yandex.ru>
2024-02-24 14:45:08 +0300

    Добавьте файлы проекта.

---
+++ PinGen/PinGen.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int pin;
+    int sum = 0;
+    cout << "Добро пожаловать в ПлюсПлюсБанк! Введите пин код : "; cin >> pin;
+
+    while (pin != 0) {
+        sum += pin % 10;
+        pin /= 10;
+        if (sum == 42) {
+            cout << "Пароль верный!"; break;
+        }
+    }
+    if (sum != 42) {
+        cout << "Пароль неверный!";
+    }
+
+
+}
